d6ae79551134e8c67b8d9de598a60f9d7e706451
DotClick <github@dotclickllc.com>
2026-01-28 06:10:23 +0500

    Update axios.ts

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+const VITE_API_BASE_URL = http://localhost:3000/api
+
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL,
+    // production url 
+    // baseURL: import.meta.env.VITE_API_BASE_URL,
+    // development url
+    baseURL: VITE_API_BASE_URL,
     headers: {
         'Content-Type': 'application/json',
     },
